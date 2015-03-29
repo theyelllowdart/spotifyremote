d8eb4766cbb887c6c907b1a3c2bfd9c7f3ecb68d
aaron <aaron@foursquare.com>
2015-03-28 20:26:56 -0400

    namespace events

--- content_script.js
+++ content_script.js
@@ -3,7 +3,7 @@ var spotifyPlayer = document.getElementById("spotify-player");
 if (spotifyPlayer) {
   chrome.runtime.onMessage.addListener(function (request) {
     if (request.type === 'ready') {
-      var evt = new CustomEvent("spotifyRemote.READY");
+      var evt = new CustomEvent("spotifyRemote.ready");
       spotifyPlayer.dispatchEvent(evt);
     } else if (request.type === 'playModeChangedEvent') {
       var evt = new CustomEvent("spotifyRemote.playModeChanged");
@@ -15,11 +15,11 @@ if (spotifyPlayer) {
     chrome.runtime.sendMessage({type: 'spotifyLoaded'});
   });
 
-  spotifyPlayer.addEventListener('play', function (event) {
+  spotifyPlayer.addEventListener('spotifyRemote.play', function (event) {
     chrome.runtime.sendMessage({type: 'play', song: event.detail});
   });
 
-  spotifyPlayer.addEventListener('pause', function () {
+  spotifyPlayer.addEventListener('spotifyRemote.pause', function () {
     chrome.runtime.sendMessage({type: 'pause'});
   });
 }
