5997c56215ac27adfd2f1ff8e8483c19dec5ff8d
aaron <theyelllowdart@gmail.com>
2015-12-20 21:30:04 -0500

    various fixes
    scan spotify port range, with retry logic
    use random subdomain to allow for connections
    fixed onAlarm spurious handshakes
    allow for consumer initiated handshakes

--- background.js
+++ background.js
@@ -13,16 +13,49 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
   ["blocking", "requestHeaders"]
 );
 
+var SPOTIFY_PORT_START = 4370;
+var SPOTIFY_PORT_END = 4381;
+var MAX_RETRY_COUNT = 3;
+
 var csrf;
 var oauth;
+var spotifyLocalPort;
 var spotifyRemote = $.Deferred();
 
+function getSpotifyLocalSubdomain() {
+  var subdomain = "";
+  for (var i = 0; i < 10; i++) {
+    subdomain += String.fromCharCode(Math.floor((Math.random() * 26 + 97)));
+  }
+  return subdomain
+}
+
 function getCSRF() {
-  return $.ajax("https://blah.spotilocal.com:4371/simplecsrf/token.json", {cache: false})
+  var csrfDeferred = $.Deferred();
+  getCSRFInternal(SPOTIFY_PORT_START, 0, csrfDeferred);
+  return csrfDeferred.promise();
+}
+
+function getCSRFInternal(port, retryCount, csrfDeferred) {
+  $.ajax("https://" + getSpotifyLocalSubdomain() + ".spotilocal.com:" + port + "/simplecsrf/token.json", {cache: false})
     .success(function (res) {
       csrf = res.token;
+      spotifyLocalPort = port;
+      csrfDeferred.resolve();
+    })
+    .fail(function () {
+      if (retryCount === MAX_RETRY_COUNT) {
+        port = port + 1;
+        retryCount = 0;
+      } else {
+        retryCount = retryCount + 1;
+      }
+      if (port < SPOTIFY_PORT_END) {
+        getCSRFInternal(port, retryCount, csrfDeferred)
+      }
     });
 }
+
 function getOAuth() {
   return $.ajax("https://open.spotify.com/token", {cache: false, data: {'_': Date.now()}})
     .success(function (res) {
@@ -30,34 +63,46 @@ function getOAuth() {
     });
 }
 
-$.when(getCSRF(), getOAuth()).then(function () {
-  spotifyRemote.resolve();
-});
+function handShake() {
+  return $.when(getCSRF(), getOAuth()).then(function () {
+    spotifyRemote.resolve();
+  });
+}
+handShake();
 
-chrome.alarms.create({periodInMinutes: 60});
-chrome.alarms.onAlarm.addListener(function () {
-  getCSRF();
-  getOAuth();
+chrome.alarms.create("handshake", {periodInMinutes: 60});
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  if (alarm.name === "handshake") {
+    handShake();
+  }
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender) {
-  spotifyRemote.then(function() {
-    if (request.type === 'play') {
-      $.ajax("https://blah.spotilocal.com:4371/remote/play.json", {
-        data: {
-          csrf: csrf,
-          oauth: oauth,
-          uri: request.song
-        }
-      });
-    } else if (request.type === 'pause') {
-      $.ajax("https://blah.spotilocal.com:4371/remote/pause.json", {
-        data: {
-          csrf: csrf,
-          oauth: oauth,
-          pause: true
-        }
-      });
-    }
-  });
+  if (request.type === 'handshake') {
+    handShake().then(function() {
+      chrome.tabs.sendMessage(sender.tab.id, request)
+    });
+  } else {
+    spotifyRemote.then(function () {
+      if (request.type === 'play') {
+        $.ajax("https://" + getSpotifyLocalSubdomain() + ".spotilocal.com:" + spotifyLocalPort + "/remote/play.json", {
+          cache: false,
+          data: {
+            csrf: csrf,
+            oauth: oauth,
+            uri: request.song
+          }
+        });
+      } else if (request.type === 'pause') {
+        $.ajax("https://" + getSpotifyLocalSubdomain() + ".spotilocal.com:" + spotifyLocalPort + "/remote/pause.json", {
+          cache: false,
+          data: {
+            csrf: csrf,
+            oauth: oauth,
+            pause: true
+          }
+        });
+      }
+    });
+  }
 });
--- content_script.js
+++ content_script.js
@@ -1,8 +1,19 @@
 var spotifyPlayer = document.getElementById("spotify-player");
 
 if (spotifyPlayer) {
+  chrome.runtime.onMessage.addListener(function (request) {
+    if (request.type === 'handshake') {
+      var evt = new CustomEvent("spotifyConsumer.handshake");
+      spotifyPlayer.dispatchEvent(evt);
+    }
+  });
+
   var evt = new CustomEvent("spotifyRemote.ready");
   spotifyPlayer.dispatchEvent(evt);
+
+  spotifyPlayer.addEventListener('spotifyRemote.handshake', function () {
+    chrome.runtime.sendMessage({type: 'handshake'});
+  });
 
   spotifyPlayer.addEventListener('spotifyRemote.play', function (event) {
     chrome.runtime.sendMessage({type: 'play', song: event.detail});
